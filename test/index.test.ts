053d896fb4f59e036309352419f6f92e45053e08
花开半亩地 <2572468699@qq.com>
2022-07-24 01:30:28 +0800

    chore: test folder move

---
+++ test/index.test.ts
@@ -0,0 +1,15 @@
+import { test, expect } from 'vitest'
+import { dotPlaceReg } from '../src/constants'
+
+import fixDot from '../src/index'
+test('前后中文,中间标点符号', async () => {
+	const testStr = `<script lang="ts" setup>
+    import { ref } from 'vue'
+    const skillsList = ref<string[]>([
+      '熟练掌握 Javascript,了解部分底层机制,如事件循环,任务调度等.',
+    ])
+  </script>`
+
+	const res = await fixDot(testStr)
+  expect(res && !dotPlaceReg.test(res)).toBe(true)
+}, 100)
---
+++ test/withCode/index.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from 'vitest'
+import fs from 'fs'
+import path from 'path'
+import fixDot from '../../src/fixDot'
+import { dotPlaceReg } from '../../src/constants'
+
+test('长文本,并且包含code1', () => {
+	fs.readFile(path.resolve(__dirname, './text.txt'), async (err: any, data: Buffer) => {
+		if (err) {
+			return console.error(err)
+		}
+		const code = data.toString()
+		let { res } = await fixDot(code)
+		expect(res && !dotPlaceReg.test(res)).toBe(true)
+		// fs.writeFileSync(path.resolve(__dirname, './res.txt'), res || '---error---')
+	})
+}) 
+ 

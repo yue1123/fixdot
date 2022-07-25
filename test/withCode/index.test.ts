3953f2b2be82a85d4424c62f41682e57756104bc
花开半亩地 <2572468699@qq.com>
2022-07-25 11:00:25 +0800

    feat: 0.0.1 功能基本完善

--- test/withCode/index.test.ts
+++ test/withCode/index.test.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import fixDot from '../../src/fixDot'
 import { dotPlaceReg } from '../../src/constants'
 
-test('长文本,并且包含code1', () => {
+test('长文本,并且包含code', () => {
 	fs.readFile(path.resolve(__dirname, './text.txt'), async (err: any, data: Buffer) => {
 		if (err) {
 			return console.error(err)

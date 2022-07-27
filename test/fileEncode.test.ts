bfd0dcb35e89f68cee5f12a22483a2b9e875dbb7
花开半亩地 <2572468699@qq.com>
2022-07-27 18:16:00 +0800

    test: 文件编码获取

---
+++ test/fileEncode.test.ts
@@ -0,0 +1,46 @@
+import jschardet from 'jschardet'
+import { test, expect } from 'vitest'
+import { writeFile, readFile } from 'fs'
+
+test('获取文件编码', async () => {
+	const path = './test/test.txt'
+	const buffer = await new Promise<Buffer>((resolve, reject) => {
+		readFile(path, (err: any, data) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data)
+			}
+		})
+	})
+	expect(jschardet.detect(buffer).encoding).toBe('UTF-8')
+})
+
+test('获取无后缀文件编码', async () => {
+	const path = './LICENSE'
+	const buffer = await new Promise<Buffer>((resolve, reject) => {
+		readFile(path, (err: any, data) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data)
+			}
+		})
+	})
+	expect(jschardet.detect(buffer).encoding).toBe('ascii')
+})
+
+
+test('获取无后缀文件编码', async () => {
+	const path = './package.json'
+	const buffer = await new Promise<Buffer>((resolve, reject) => {
+		readFile(path, (err: any, data) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data)
+			}
+		})
+	})
+	expect(jschardet.detect(buffer).encoding).toBe('ascii')
+})
